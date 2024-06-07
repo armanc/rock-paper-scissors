64db88e291e24b69cb462c33666e03afe8bbc1d8
Armands Baris <armands.baris@gmail.com>
2024-06-07 13:46:27 +0300

    git ignore

---
+++ index.ts
@@ -0,0 +1,9 @@
+const prompots = require('prompts');
+
+(async () => {
+    const response = await prompots({
+        type: 'text',
+        name: 'value',
+        message: 'Select '
+    })
+})();
